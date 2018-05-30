dd47a26158a75961b717445ad2a67aa3ba1ab7ca
Mikael Luz <mconceicaos@indracompany.com>
2018-05-30 14:53:15 -0300

    Da

---
+++ repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, soma;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            soma = n1 + n2;
+
+            Console.WriteLine("O resulado é: "+soma);
+
+            Console.ReadKey();
+
+        }
+    }
+}
